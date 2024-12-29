eef7c318c73ad39a2497c96bf416a505eb52297f
HiDoRo-Gilete <127959236+HiDoRo-Gilete@users.noreply.github.com>
2024-12-29 19:51:50 +0700

    create framwork again

--- my-react-app-learning1/src/main.jsx
+++ my-react-app-learning1/src/main.jsx
@@ -1,32 +1,7 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
 
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Title from "./pages/Title";
-import InforUser from "./pages/InforUser";
-
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="title" element={<Title />} />
-        <Route path="infor_user" element={<InforUser />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
