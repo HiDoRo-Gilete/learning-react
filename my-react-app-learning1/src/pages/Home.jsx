b022a13f120f156894a6dd5c3bfb15ab135d6ad3
HiDoRo-Gilete <127959236+HiDoRo-Gilete@users.noreply.github.com>
2024-12-29 20:56:04 +0700

    reacte css file

--- my-react-app-learning1/src/pages/Home.jsx
+++ my-react-app-learning1/src/pages/Home.jsx
@@ -1,12 +1,17 @@
 import '../css/Home.css'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Home = () => {
     return(
       <>
-        <h1>Home</h1>;
+        <h1>Home</h1>
+        
       </> 
     )
   };
   
   export default Home;
+
+
+ // Import cấu hình FontAwesome
